7b1e0adeeb2ad5606e7082be122d1d1cad42d166
Reza <rezabaiust@gmail.com>
2021-12-13 01:23:28 +0600

    Bazar list completed with add and remove functionality

--- bazar_list/app.js
+++ bazar_list/app.js
@@ -8,5 +8,26 @@ new Vue({
             {name: 'oil', price: 22.50},
         ],
         newItem: '',
-    }
+    },
+    computed:{
+        total() {
+            var total = 0
+            this.items.forEach(item => {
+                total += parseFloat(item.price)
+            })
+            return total
+        }
+    },
+    methods: {
+        addItem() {
+            this.items.push({
+                name: this.newItem,
+                price: 0.00
+            })
+            this.newItem = "";
+        },
+        removeItem(index) {
+            this.items.splice(index, 1)
+        }
+    },
 });
