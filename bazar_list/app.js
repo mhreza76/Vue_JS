584d7d7037fc20df407b5761b2dc1e546d7e5d9d
Reza <rezabaiust@gmail.com>
2021-12-13 00:18:34 +0600

    Bazar List mini Project

---
+++ bazar_list/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#app",
+    data: {
+        title: "Vue Basics",
+        items: [
+            {name: 'rice', price: 12.50},
+            {name: 'wheat', price: 15.50},
+            {name: 'oil', price: 22.50},
+        ],
+        newItem: '',
+    }
+});
